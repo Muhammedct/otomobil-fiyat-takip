1873f03eff0c170573a035a16d5a09b1d75ca7ba
Muhammedct <m.topcu9696@gmail.com>
2025-10-16 16:40:16 +0300

    Update base_scraper.py

--- scrapers/base_scraper.py
+++ scrapers/base_scraper.py
@@ -2,17 +2,8 @@ import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from bs4 import BeautifulSoup
-import os
 
 class BaseScraper:
-    def __init__(self, url):
-        self.url = url
-        self.driver = self._initialize_driver()
-        self.data = []
 
     def _initialize_driver(self):
         """GitHub Actions için özel Chrome/WebDriver başlatma."""
@@ -24,25 +15,17 @@ class BaseScraper:
 
         chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("--window-size=1920,1080")
+        chrome_options.add_argument("--blink-settings=imagesEnabled=false")
+        chrome_options.add_argument("--disable-extensions")
+        chrome_options.add_argument("--dns-prefetch-disable")
+        chrome_options.add_argument("--disable-setuid-sandbox")
+        chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
+
 
         service = Service(service_log_path=os.devnull)
 
         try:
             driver = webdriver.Chrome(service=service, options=chrome_options)
-            driver.set_page_load_timeout(45)
+            driver.set_page_load_timeout(60)
             return driver
         except Exception as e:
-            print(f"WebDriver başlatılamadı. Chrome kurulu ve PATH'e ekli mi? Hata: {e}")
-            return None
-
-    def scrape(self):
-        """Bu metodun alt sınıflarda uygulanması zorunludur."""
-        raise NotImplementedError("Bu metod alt sınıflar tarafından uygulanmalıdır.")
-
-    def get_dataframe(self):
-        return pd.DataFrame(self.data)
-
-    def __del__(self):
-        """Scraper nesnesi silinirken tarayıcıyı kapat."""
-        if self.driver:
-            self.driver.quit()
